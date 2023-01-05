8a30b280ca86b7ca53e1ba6fb1edb0efc2d37b88
Zehra İkizler <zehraaikizler@gmail.com>
2023-01-05 16:39:43 +0300

    formik added

--- components/Reservation.jsx
+++ components/Reservation.jsx
@@ -1,38 +1,58 @@
 import React from "react";
 import Input from "./form/Input";
 import Title from "./ui/Title";
+import { useFormik } from "formik";
 
 const Reservation = () => {
+  const onSubmit = async (values, actions) => {
+    await new Promise((resolve) => setTimeout(resolve, 4000));
+    actions.resetForm();
+  };
+  const { values, handleChange, handleSubmit } = useFormik({
+    initialValues: {
+      fullName: "",
+      phoneNumber: "",
+      email: "",
+      persons: "",
+      date: "",
+    },
+    onSubmit,
+  });
+
   const inputs = [
     {
       id: 1,
       name: "fullName",
       type: "text",
       placeholder: "Your Full Name",
+      value: values.fullName,
     },
     {
       id: 2,
       name: "phoneNumber",
       type: "number",
       placeholder: "Your Phone Number",
+      value: values.phoneNumber,
     },
     {
       id: 3,
       name: "email",
       type: "email",
       placeholder: "Your Email Address",
+      value: values.email,
     },
     {
       id: 4,
       name: "persons",
       type: "number",
       placeholder: "How Many Persons?",
+      value: values.persons,
     },
     {
       id: 5,
-      name: "persons",
+      name: "date",
       type: "datetime-local",
-      placeholder: "How Many Persons?",
+      value: values.date,
     },
   ];
 
@@ -40,14 +60,16 @@ const Reservation = () => {
     <div className="container mx-auto py-12">
       <Title addClass="text-[40px] mb-10">Book A Table</Title>
       <div className="flex justify-between flex-wrap-reverse gap-x-10 gap-y-6">
-        <div className="md:flex-1 w-full">
+        <form className="md:flex-1 w-full" onSubmit={handleSubmit}>
           <div className="flex flex-col gap-y-3">
             {inputs.map((input) => (
-              <Input key={input.id} {...input} />
+              <Input key={input.id} {...input} onChange={handleChange} />
             ))}
           </div>
-          <button className="btn-primary mt-4">BOOK NOW</button>
-        </div>
+          <button className="btn-primary mt-4" type="submit">
+            BOOK NOW
+          </button>
+        </form>
         <div className="md:flex-1 w-full">
           <iframe
             className="rounded-sm"
